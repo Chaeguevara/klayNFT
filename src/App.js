9c4d1b3b4ac877e34752c0a6f916fbce2abe41dc
Chaeguevara <dfso2@naver.com>
2022-02-28 14:53:21 +0900

    put pirvate key in env file and read data via caver
     As the private key must be kept in secret, it is stored in env file and read by relative location. Some methods are implemented to read data from smart contract. The address is hard coded. Presumably, by replacing ACCESS_KEY_ID and SECRET_ACCESS_KEY with yours, it will work whereever the environment is.
    
    - read data from smart contract via caver-js
    - secret keys are hidden by .env file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import Caver from 'caver-js';
 
+require('dotenv').config();
+
+const COUNT_CONTRACT_ADDRESS='0xb2A2e9C62100AE977f57fd6F16d98cD5919430c7';
+const ACCESS_KEY_ID = process.env.REACT_APP_ACCESS_KEY_ID;
+const SECRET_ACCESS_KEY = process.env.REACT_APP_SECRET_ACCESS_KEY;
+const COUNT_ABI = '[ { "constant": true, "inputs": [], "name": "count", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "getBlockNumber", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_count", "type": "uint256" } ], "name": "setCount", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" } ]';
+const CHAIND_ID = '1001'; //Baobab <-> Mainnet 8217
+
+const option={
+  headers:[
+    {
+    name: "Authorization",
+    value: "Basic " + Buffer.from(ACCESS_KEY_ID +":" + SECRET_ACCESS_KEY).toString("base64")
+  },{
+    name: "x-chain-id",
+    value: CHAIND_ID
+  }
+  ]
+}
+
+const caver = new Caver(new Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn",option));
+const CountContract = new caver.contract(JSON.parse(COUNT_ABI), COUNT_CONTRACT_ADDRESS);
+const readCount= async() =>{
+  const _count = await CountContract.methods.count().call();
+  console.log(_count);
+}
+
+const getBalance = (address)  => {
+  return caver.rpc.klay.getBalance(address).then((res) =>{
+    const balance = caver.utils.convertFromPeb(caver.utils.hexToNumberString(res));
+    console.log(`BALANCE: ${balance}`);
+    return balance;
+  })
+}
+// 1. Smart contract 배포 주소 가져오기
+// 2. caver.js 이용해서 스마트 컨트랙트 연동하기
+// 3. 가져온 스마트 컨트랙트 실해결과 웹에 표현하기
 function App() {
+  console.log(ACCESS_KEY_ID);
+  console.log(SECRET_ACCESS_KEY);
+
+  readCount();
+  getBalance("0x03e4168d8133eb91ae850bc1d7985d76d7ec295c");
   return (
     <div className="App">
       <header className="App-header">
