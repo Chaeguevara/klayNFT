5c76c275d4550d9ab18538ea70014208d032d7e4
Chaeguevara <dfso2@naver.com>
2022-03-06 20:09:36 +0900

    Connect klip wallet via QR code
    By utilziing several klip api and state in react, React app query the
    address of who scanned the qrcode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from "./logo.svg";
 import { getBalance, setCount } from "./api/UseCaver";
+import QRCode from "qrcode.react";
+import * as KlipApi from "./api/UseKlip";
 import "./App.css";
 import React, { useState } from "react";
 
@@ -10,33 +12,35 @@ const PRIVATE_KEY = process.env.REACT_APP_PRIVATE_KEY;
 // 1. Smart contract 배포 주소 가져오기
 // 2. caver.js 이용해서 스마트 컨트랙트 연동하기
 // 3. 가져온 스마트 컨트랙트 실해결과 웹에 표현하기
+const DEFAUT_QR_CODE = "DEFAULT";
+const onPressButton2 = (_balance, _setBalance) => {
+  _setBalance(_balance);
+};
 function App() {
   //console.log(ACCESS_KEY_ID);
   //console.log(SECRET_ACCESS_KEY);
+  const [qrvalue, setQrValue] = useState(DEFAUT_QR_CODE);
   const [balance, setBalance] = useState("0");
-  const onPressButton2 = (balance) => {
-    //console.log("hi!!");
-    setBalance("10");
-  };
   //readCount();
   getBalance("0x03e4168d8133eb91ae850bc1d7985d76d7ec295c");
+  const onClickGetAddress = () => {
+    KlipApi.getAddress(setQrValue);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button
-          title={"Set Count!!"}
-          onClick={() => {
-            setCount(1);
-          }}
-        />
         <button
           onClick={() => {
-            onPressButton2();
+            onClickGetAddress();
           }}
         >
-          Hi
+          주소가져오기
         </button>
+        <br />
+        <br />
+        <br />
+        <br />
+        <QRCode value={qrvalue} />
         <p>{balance}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
